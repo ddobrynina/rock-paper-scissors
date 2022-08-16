a9129a9f949d64b3055e03ebafa8a846caf0e836
ddobrynina <ddobrynina924@gmail.com>
2022-08-16 16:17:35 -0400

    Add buttons for game intead of asking for user input

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice()
 {
     let number = Math.floor(Math.random() * 3);
@@ -20,65 +22,66 @@ function getComputerChoice()
 
 function playRound(computerSelection, playerSelection)
 {
-    playerSelection = playerSelection.toLowerCase();
     let win = 0;
+    const container = document.querySelector('#container');
+    const content = document.createElement('div');
+    content.classList.add('content');
+
     if(computerSelection===playerSelection)
     {
-        alert("You Tie!");
+        
+        content.textContent = "You Tie!";
         win = 0.5;
     }
     else if(computerSelection === "rock" && playerSelection === "scissors")
     {
-        alert("You lose! Rock beats scissors!");
+        content.textContent = "You lose! Rock beats scissors!";
     }
     else if(computerSelection === "scissors" && playerSelection ==="paper")
     {
-        alert("You lose! Scissors beats paper!");
+        content.textContent = "You lose! Scissors beats paper!";
     }
     else if(computerSelection === "paper" && playerSelection === "rock")
     {
-         alert("You lose! Paper beats rock!");
+        content.textContent = "You lose! Paper beats rock!";
     }
     else if(computerSelection === "scissors" && playerSelection === "rock")
     {
-        alert("You win! Rock beats scissors!");
+        content.textContent = "You win! Rock beats scissors!";
         win = 1;
     }
     else if(computerSelection === "rock" && playerSelection === "paper")
     {
-        alert("You win! Paper beats rock!");
+        content.textContent = "You win! Paper beats rock!";
         win = 1;
     }
     else if(computerSelection === "paper" && playerSelection === "scissors")
     {
-        alert("You win! Scissors beats paper!");
+        content.textContent = "You win! Scissors beats paper!";
         win = 1;
     }
+    container.appendChild(content);
     return win
 }
 
-function game()
-{
-    let winCount = 0;
-    
-    for (let i = 0; i < 5; i++) 
+function game(winCount)
+{ 
+    const container = document.querySelector('#container');
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    if(winCount%5 <2.5)
     {
-        let playerSelection =  window.prompt("Rock, Paper, Scissors?");
-        let computerSelection = getComputerChoice();
-        let win = playRound(computerSelection,playerSelection);
-        winCount+=win;
-        console.log(winCount);
-    } 
-    if(winCount<2.5)
-    {
-        alert("You lost!");
+        content.textContent = "You lost!";
     }
-    else if(winCount === 2.5)
+    else if(winCount%5 === 2.5)
     {
-        alert("You tied!");
+        content.textContent = "You tied!";
     }
-    else if(winCount>2.5)
+    else if(winCount%5 >2.5)
     {
-        alert("You won!");
+        content.textContent = "You won!";
     }
+    container.appendChild(content);
 }
+
